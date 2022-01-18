149c1ebc1f232e53b9508bb4a0943cb75a9e0320
Michael Ripperger <michael.ripperger@swri.org>
2022-01-18 11:36:44 -0600

    Moved scanner params into simulator header

--- include/simulated_lidar_scanner/lidar_scanner_simulator.h
+++ include/simulated_lidar_scanner/lidar_scanner_simulator.h
@@ -1,18 +1,37 @@
-#ifndef LIDAR_SCANNER_SIMULATOR_H
-#define LIDAR_SCANNER_SIMULATOR_H
+#pragma once
 
 #include <simulated_lidar_scanner/synthetic_lidar_scanner/vtkLidarScanner.h>
-#include <simulated_lidar_scanner/scanner_params.h>
+
 #include <vtkSmartPointer.h>
 #include <vtkPolyData.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
+struct ScannerParams
+{
+  /** @brief Horizontal span of the field of view of the sensor (rad) */
+  double theta_span;
+  /** @brief Vertical span of the field of view of the sensor (rad) */
+  double phi_span;
+  /** @brief Sensor noise along the line of sight (LOS) direction (m)*/
+  double los_variance;
+  /** @brief Sensor noise in the direction orthogonal to the line of sight (m) */
+  double orthogonal_variance;
+  /** @brief Maximum angle of incidence to a surface that will result in data (rad). */
+  double max_incidence_angle;
+  /** @brief Maxmium measurement distance of the sensor (m) */
+  double max_distance;
+  /** @brief Number of sensor data points in the horizontal (theta) field of view span of the sensor */
+  int theta_points;
+  /** @brief Number of sensor data points in the vertical (phi) field of view span of the sensor */
+  int phi_points;
+};
+
 class LidarScannerSim
 {
 public:
-  LidarScannerSim(const scanner_params& sim);
+  LidarScannerSim(const ScannerParams& sim);
 
   inline void setScannerScene(const vtkSmartPointer<vtkPolyData> &scene)
   {
@@ -34,7 +53,6 @@ public:
   }
 
 private:
-
   vtkSmartPointer<vtkLidarScanner> scanner_;
   vtkSmartPointer<vtkPolyData> scan_data_vtk_;
   pcl::PointCloud<pcl::PointNormal>::Ptr scan_data_cloud_;
@@ -44,5 +62,3 @@ private:
   bool enable_distance_filter_ = false;
   double max_distance_;
 };
-
-#endif // LIDAR_SCANNER_SIMULATOR_H
